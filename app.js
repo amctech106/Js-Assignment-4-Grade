9144598fdc2f780c565e8fedda8a09fb7e212deb
amctech106 <121874402+amctech106@users.noreply.github.com>
2026-06-30 23:02:55 +0500

    done

--- app.js
+++ app.js
@@ -5,18 +5,19 @@ let result = document.querySelector("#result");
 btn.addEventListener("click", () => {
   let inp = parseFloat(input.value);
 
-  if (inp > 90 && inp <100) {
+  if (inp > 90 && inp <= 100) {
     result.textContent = "اے پلس گریڈ";
-  } else if (inp > 80 && inp <100) {
+  } else if (inp > 80 && inp < 100) {
     result.textContent = "اے گریڈ";
-  } else if (inp > 70 && inp <100) {
+  } else if (inp > 70 && inp < 100) {
     result.textContent = "بی گریڈ";
-  } else if (inp > 60 && inp <100) {
+  } else if (inp > 60 && inp < 100) {
     result.textContent = "سی گریڈ";
-  } else if (inp > 50 && inp <100) {
+  } else if (inp > 50 && inp < 100) {
     result.textContent = "ڈی گریڈ";
   } else if (inp > 100 || inp <= 0) {
-    result.textContent = "آپ نے غلط نمبر درج کیا ہے برائے مہربانی ایک سے سو تک نمبر درج کریں ";
+    result.textContent =
+      "آپ نے غلط نمبر درج کیا ہے برائے مہربانی ایک سے سو تک نمبر درج کریں ";
   } else {
     result.textContent = "ناکام";
   }
